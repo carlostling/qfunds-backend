41545b630a124fbe776fa24831faa66a71ab91f1
Carl Östling <31474054+carlostling@users.noreply.github.com>
2022-03-16 14:15:38 +0100

    switch to maven...

---
+++ src/test/java/com/qfunds/QfundsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qfunds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QfundsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
